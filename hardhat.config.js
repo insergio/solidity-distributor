dca73c16743651b9339011ae9f75a0541de60f69
insergio <sergiodelahoz26@gmail.com>
2021-10-13 14:33:07 -0500

    added .env support

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
-
+const dotenv = require('dotenv');
+dotenv.config();
+let {RINKEBY_ACCOUNT, RINKEBY_URL} = process.env
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -18,4 +20,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: RINKEBY_URL, //Infura url with projectId
+      accounts: [RINKEBY_ACCOUNT] // add the account that will deploy the contract (private key)
+     },
+   }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -15,7 +15,7 @@ async function main() {
 
   // We get the contract to deploy
   const Distributor = await hre.ethers.getContractFactory("Distributor");
-  const distributor = await Distributor.deploy("Hello, Hardhat!");
+  const distributor = await Distributor.deploy();
 
   await distributor.deployed();
 
@@ -24,9 +24,7 @@ async function main() {
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
